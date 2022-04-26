1ed32682c459a96fce2697e22c668216a54dc662
TaherJerbi <jerbi.taherr@gmail.com>
2022-04-27 00:48:08 +0100

    fix: User.todos field resolver

--- resolvers/User.js
+++ resolvers/User.js
@@ -2,8 +2,6 @@ import {db} from "../data/db.js";
 
 export const User = {
     todos: ( user ) => {
-        return db.todos.find(
-            (todo) => todo.user == user.id
-        );
+        return db.todos.filter((todo) => todo.user == user.id);
     }
 }
