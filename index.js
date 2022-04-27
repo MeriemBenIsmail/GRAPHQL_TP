d78c10b0d53116a9c3e8708dad56aeedc3211155
TaherJerbi <jerbi.taherr@gmail.com>
2022-04-27 16:08:43 +0100

    feat: create / update / delete todos

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import { Student } from './resolvers/Student.js'
 import { Todo } from './resolvers/Todo.js';
 import { User } from './resolvers/User.js';
 import { db } from "./data/db.js";
+import { Mutation } from "./resolvers/Mutation.js";
 // ... or using "require()"
 // const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = "schema/schema.graphql";
 const resolvers = {
-    Query,
-    Student,
-    Todo,
-    User
+  Query,
+  Student,
+  Todo,
+  User,
+  Mutation,
 };
 const server = new GraphQLServer({ typeDefs, resolvers, context: { db } });
 server.start(() => console.log('Server is running on localhost:4000'))
---
+++ resolvers/Mutation.js
@@ -0,0 +1,38 @@
+import { v4 as uuidv4 } from "uuid";
+export const Mutation = {
+  createTodo: (_parent, { data }, { db }) => {
+    if (!db.users.find((u) => u.id == data.user)) {
+      throw new Error("User does not exist.");
+    }
+    const newTodo = {
+      id: uuidv4(),
+      ...data,
+      status: data?.status ?? "WAITING",
+    };
+    db.todos.push(newTodo);
+    return newTodo;
+  },
+
+  updateTodo: (_parent, { data }, { db, pubsub }) => {
+    let updatedTodo = updateTodoInput;
+
+    const updatedTodos = db.todos.map((todo) => {
+      if (todo.id === updateTodoInput.id) {
+        updatedTodo = {
+          id: updateTodoInput.id,
+          ...data,
+        };
+        return updateTodo1;
+      } else return todo;
+    });
+    db.todos = updatedTodos;
+    return updatedTodo;
+  },
+
+  deleteTodo: (_parent, { todoId }, { db }) => {
+    const deletedTodo = db.todos.find((todo) => todo.id === todoId);
+
+    db.todos = db.todos.filter((todo) => todo.id !== todoId);
+    return deletedTodo;
+  },
+};
